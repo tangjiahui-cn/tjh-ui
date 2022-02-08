2d2749309772675aeb3c3dd4f14899e5293fb046
tangjiahui <tjhaaa@163.com>
2022-02-08 19:08:23 +0800

    fix(rootResolve)：修复win10下打包路径不正确的问题。

--- build/utils/rootResolve.js
+++ build/utils/rootResolve.js
@@ -1,11 +1,6 @@
-// import * as path from "path";
 const path = require("path");
 
 module.exports = function (importPath) {
-  const rootPath = process.cwd().replace(/\\/g, "/");
-  return path.resolve(rootPath, importPath);
-}
-// export default function (importPath) {
-//   const rootPath = process.cwd().replace(/\\/g, "/");
-//   return path.resolve(rootPath, importPath);
-// }
+  return path.resolve(process.cwd(), importPath)
+    .replace(/\\/g, "/");
+};
