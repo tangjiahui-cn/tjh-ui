aef9aadf02be8d2f026f77c0f35f7b3ff024d5ab
tangjiahui <tjhaaa@163.com>
2022-02-09 18:18:40 +0800

    fix(rollup.lib.js):修改对应测试文件。

--- build/__tests__/rollup.lib.spec.js
+++ build/__tests__/rollup.lib.spec.js
@@ -2,8 +2,8 @@ import {getInput, getFile, getName} from "../rollup.lib"
 import {name} from "../../package.json"
 
 test("test lib", () => {
-  expect(getInput()).toBe("packages/index.ts")
-  expect(getInput("button")).toBe("packages/button/index.ts")
+  expect(getInput()).toBe("packages/index.js")
+  expect(getInput("button")).toBe("packages/button/index.js")
 
   expect(getFile("", "umd")).toBe("lib/index.umd.js")
   expect(getFile("button", "umd")).toBe("lib/button/index.umd.js")
