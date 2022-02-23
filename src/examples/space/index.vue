6f83f8ac548d34caf83b797868282add72603c7f
tangjiahui <tjhaaa@163.com>
2022-02-23 23:56:01 +0800

    fix(example-space): 修改示例点击按钮位置方便观察

--- src/examples/space/index.vue
+++ src/examples/space/index.vue
@@ -2,13 +2,13 @@
   <Page title="间距（Space）">
     <Block v-if=true title="1、间距大小(size)">
       <t-space :size="size">
+        <t-button type="primary" @click="size += 10">size + 10</t-button>
+        <t-button type="primary" @click="size -= 10">size - 10</t-button>
         <t-button type="primary"></t-button>
         <t-button type="default"></t-button>
         <t-button type="dashed"></t-button>
         <t-button type="text"></t-button>
         <t-input allowClear/>
-        <t-button type="primary" @click="size += 10">size + 10</t-button>
-        <t-button type="primary" @click="size -= 10">size - 10</t-button>
       </t-space>
     </Block>
   </Page>
